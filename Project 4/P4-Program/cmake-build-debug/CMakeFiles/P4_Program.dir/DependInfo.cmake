357b3e6e02b9f0e58007c3f5ba48e7aeaaf4ac1d
Alexander Swanson <alexander.swanson.1@uvm.edu>
2018-04-13 08:50:15 -0400

    Uploading Project 5 files.

---
+++ Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/CustomSort.cpp" "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/CustomSort.cpp.o"
+  "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/RedditElement.cpp" "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/RedditElement.cpp.o"
+  "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/Sort.cpp" "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/Sort.cpp.o"
+  "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/main.cpp" "/Users/admin/Documents/University/2017 - 2018/Spring/CS 124/Projects/Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Project 4/P4-Program/cmake-build-debug/CMakeFiles/P4_Program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P4_Program.dir/main.cpp.o"
+  "CMakeFiles/P4_Program.dir/RedditElement.cpp.o"
+  "CMakeFiles/P4_Program.dir/Sort.cpp.o"
+  "CMakeFiles/P4_Program.dir/CustomSort.cpp.o"
+  "P4_Program.pdb"
+  "P4_Program"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P4_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
